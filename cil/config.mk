b97adac6f27d22d5d20daa37e63e44dc1c81e8dd
asimkadav <asimkadav@gmail.com>
2012-07-29 09:53:22 -0500

    initial commit

---
+++ cil/config.mk
@@ -0,0 +1,6 @@
+# A bunch of variables  -*- Mode: makefile -*-
+export ARCHOS=x86_LINUX
+export HAS_MSVC=no
+export CILHOME=/u/k/a/kadav/public/html/software/cil
+export DEFAULT_COMPILER=_GNUCC
+export _GNUCC=1
